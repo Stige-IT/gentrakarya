a45d1acf1f79fa06c83d5a84e32f8b4cacc18576
Andri10dev <andri10.dev@gmail.com>
2024-04-02 09:58:01 +0700

    fix base url

--- src/services/base_url.js
+++ src/services/base_url.js
@@ -1,2 +1,2 @@
-export const BaseURL = 'http://127.0.0.1:8000/'
-// export const BaseURL = 'https://begentrakarya.stigeit.com/'
+// export const BaseURL = 'http://127.0.0.1:8000/'
+export const BaseURL = 'https://begentrakarya.stigeit.com/'
