ec41475e932b8d1895ad01923224dda94b593579
Andri10dev <andri10.dev@gmail.com>
2024-04-02 10:36:31 +0700

    fix responsive sample magang

--- src/views/landing_page/sections/sample_magang.jsx
+++ src/views/landing_page/sections/sample_magang.jsx
@@ -3,6 +3,7 @@ import Title from "../../../components/Title"
 import { getMagangGeneralSample } from "../../../services/magang_service"
 import { useState } from "react"
 import MagangCard from "../../../components/card/magang_card"
+import { Link } from "react-router-dom"
 
 const SampleMagang = () => {
 
@@ -22,7 +23,7 @@ const SampleMagang = () => {
             <div className="w-full py-10 px-5 lg:px-10 ">
                 <div className="flex justify-between">
                     <Title name="Info Magang"></Title>
-                    <a href="/magang" className="text-black bg-custom-yellow px-3 py-2 rounded-md font-semibold">Lihat Semua</a>
+                    <Link to="/magang" className="text-black bg-custom-yellow px-3 py-2 rounded-md font-semibold">Lihat Semua</Link>
                 </div>
                 <div className="w-full mt-3 h-1 rounded-full bg-black"></div>
                 <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 py-5">
